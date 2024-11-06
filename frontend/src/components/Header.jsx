73319288fdd5adf8b15f182cda92525bb4e9809a
Shashwat Pati <shashwatpati02@gmail.com>
2024-11-06 16:16:03 +0530

    Icon

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -4,32 +4,35 @@ import SmallProduct from "../pages/Products/SmallProduct";
 import ProductCarousel from "../pages/Products/ProductCarousel";
 
 const Header = () => {
-  const { data, isLoading, error } = useGetTopProductsQuery();
+    const { data, isLoading, error } = useGetTopProductsQuery();
 
-  if (isLoading) {
-    return <Loader />;
-  }
+    if (isLoading) {
+        return <Loader />;
+    }
 
-  if (error) {
-    return <h1>ERROR</h1>;
-  }
+    if (error) {
+        return <h1>ERROR</h1>;
+    }
 
-  return (
-    <>
-      <div className="flex justify-around">
-        <div className="xl:block lg:hidden md:hidden:sm:hidden">
-          <div className="grid grid-cols-2">
-            {data.map((product) => (
-              <div key={product._id}>
-                <SmallProduct product={product} />
-              </div>
-            ))}
-          </div>
-        </div>
-        <ProductCarousel />
-      </div>
-    </>
-  );
+    return (
+        <>
+            <div className="flex justify-around">
+                <div className="xl:block lg:hidden md:hidden:sm:hidden">
+                    <div className="w-[30rem] ml-[2rem] p-3 relative">
+                        Hello
+                    </div>
+                    <div className="grid grid-cols-2">
+                        {data.map((product) => (
+                            <div key={product._id}>
+                                <SmallProduct product={product} />
+                            </div>
+                        ))}
+                    </div>
+                </div>
+                <ProductCarousel />
+            </div>
+        </>
+    );
 };
 
 export default Header;
